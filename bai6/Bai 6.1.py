cbfa9d3e6f73536dd90c3c26ec5928cda96d779c
Huy <hoanghuy31126@gmail.com>
2025-12-09 21:30:39 +0700

    Add files via upload

---
+++ bai6/Bai 6.1.py
@@ -0,0 +1,12 @@
+print("Sinh vien: Nguyen Hoang Huy")
+print("MSSV: 245752021610060")
+print("######################")
+
+class Circle(object):
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return self.radius**2 * 3.14
+aCircle = Circle(2)
+print (aCircle.area())
---
+++ bai6/Bai 6.2.py
@@ -0,0 +1,13 @@
+print("Sinh vien: Nguyen Hoang Huy")
+print("MSSV: 245752021610060")
+print("######################")
+
+class Hinhchunhat(object):
+    def __init__(self, chieu_dai, chieu_rong):
+        self.chieu_dai = chieu_dai
+        self.chieu_rong = chieu_rong
+
+    def dien_tich(self):
+        return self.chieu_dai * self.chieu_rong
+hcn = Hinhchunhat(8, 5)
+print(hcn.dien_tich())
